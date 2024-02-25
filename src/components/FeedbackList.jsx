eb1fda8d4dd6cb796f066a51225baf4b722e0dd4
DataStreamViking <stedzh99@gmail.com>
2024-02-25 18:56:25 +0100

    day 2

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,12 +1,19 @@
 import { motion, AnimatePresence } from "framer-motion";
-import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./shared/Spinner";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback, handleDelete }) {
-    if (!feedback || feedback.length === 0) {
+function FeedbackList() {
+    const { feedback, isLoading } = useContext(FeedbackContext);
+
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>;
     }
-    return (
+
+    return isLoading ? (
+        <Spinner />
+    ) : (
         <div className="feedback-list">
             <AnimatePresence>
                 {feedback.map((item) => (
@@ -16,25 +23,12 @@ function FeedbackList({ feedback, handleDelete }) {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                     >
-                        <FeedbackItem
-                            key={item.id}
-                            item={item}
-                            handleDelete={handleDelete}
-                        />
+                        <FeedbackItem key={item.id} item={item} />
                     </motion.div>
                 ))}
             </AnimatePresence>
         </div>
     );
 }
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
-    ),
-};
 
 export default FeedbackList;
